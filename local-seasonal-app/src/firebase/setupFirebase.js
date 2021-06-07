8e6ff2361ef21cc368e2a99d6213b0d05d699d45
SlothyDee <slothydeelight@gmail.com>
2021-06-06 21:06:22 -0700

    first deploy

---
+++ local-seasonal-app/src/firebase/setupFirebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase/app";
+// Required for side-effects
+import "firebase/firestore";
+import "firebase/auth";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+});
